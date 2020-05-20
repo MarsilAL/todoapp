980a22d3725faf3a3f29f795c1e786dbfdcc90db
MarsilAL <alashkar.marsil@gmail.com>
2020-05-20 15:32:31 +0200

    Updates

---
+++ precache-manifest.8bcfaec7f907e28cfae0e641a0825c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a913e19d79b00cc87a32680f029eef4",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "c1d2af3e70245e93f648",
+    "url": "/todoapp/static/css/main.8ef55f48.chunk.css"
+  },
+  {
+    "revision": "5043d1a4efbf8ced5a9a",
+    "url": "/todoapp/static/js/2.e4067d59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.e4067d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d2af3e70245e93f648",
+    "url": "/todoapp/static/js/main.44de1f52.chunk.js"
+  },
+  {
+    "revision": "9b2f915f09a204b07370",
+    "url": "/todoapp/static/js/runtime-main.d779cf41.js"
+  }
+]);
